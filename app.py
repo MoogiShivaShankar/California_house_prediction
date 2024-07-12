7dd0388fd6f2ddc2e42a016d583deab0cd346c3a
MoogiShivaShankar <shivashankarmoogi@gmail.com>
2024-07-12 13:36:20 +0100

    The Web App is Ready

--- app.py
+++ app.py
@@ -45,6 +45,14 @@ def predict_api():
             return jsonify({"error": str(e)}), 400
     else:
         return jsonify({"message": "Send a POST request to this endpoint"}), 200
+    
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction price is{}".format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
